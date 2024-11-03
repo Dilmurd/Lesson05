25b17edc8bf3248e808d30f18a621774eb58d1ac
Dilmurd <0nishonov.dilmurod@gmail.com>
2024-11-03 09:14:46 +0500

    custom

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -3,6 +3,7 @@ import "./Menu.css"
 import axios from 'axios'
 import star from "../../assets/Group 85.png"
 import Loading from '../loading/Loading'
+import { useFetch } from '../../hooks/useFetch'
 
 
 const BASE_URL = "https://dummyjson.com"
@@ -10,16 +11,17 @@ const BASE_URL = "https://dummyjson.com"
 
 function Menu() {
     const [products,setProducts] = useState(null)
+    const [offset,setOffset] = useState(1)
 
     useEffect(()=>{
         axios
-          .get(`${BASE_URL}/products`)
+          .get(`${BASE_URL}/products?limit=${6 * offset}`)
           .then(res => {
             setProducts(res.data.products)
           })
           .catch(err => console.log(err))
           .finally()
-    },[])
+    },[offset])
     const productItems = products?.map((pro)=>(
         <div className='menu__card' key={pro.id}>
             <img src={pro.images[0]} className='image' alt="" />
@@ -44,6 +46,7 @@ function Menu() {
                     <p className='menu__desc'>There are many things are needed to start the Fast Food Business. You need not only Just Food Stalls with Persons but also specialized equipment, Skills to manage Customers, </p>
                 </div>
                 <div className="menu__wrapper">{productItems}</div>
+                <button onClick={()=> setOffset(p => p + 1)} className='btns'>See more</button>
             </div>
         </section>
         <Loading/>
